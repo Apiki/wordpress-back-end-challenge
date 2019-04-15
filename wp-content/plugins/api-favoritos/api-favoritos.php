e2d1f1d3196d37ef45e87b1aa61934c553c12d94
Alexandre da Silva Anjos <alexanjos87@hotmail.com>
2019-04-15 15:03:53 -0300

    Pasta do Plugin

---
+++ wp-content/plugins/api-favoritos/api-favoritos.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+Plugin Name: API-Favoritos
+Plugin URI: https://localhost
+Description: favoritar posts para usuários logados usando a WP REST API.
+Version: 1.0
+Author: Alexandre S. Anjos
+Author URI: http://localhost/
+License: GPLv2 or later
+Text Domain: API-Favoritos
+*/
+
+function apif_add_menu_admin () {
+
+}
+
+/**
+* Modificam dados feitos por funções
+*/
+//add_filter();
+
+/**
+* Adicionam/removem funções
+*/
+//add_hooks();
