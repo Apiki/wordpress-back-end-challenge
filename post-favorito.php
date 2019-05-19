41919db59c4a5ae69dc99f2ddcaa990117284657
ricardo <ricardobboy_@hotmail.com>
2019-05-18 22:19:19 -0300

    Foi adicionada criado shortcode para exibir os posts favoritos

--- post-favorito.php
+++ post-favorito.php
@@ -40,12 +40,115 @@ add_action('plugins_loaded', function() {
         return;
     }
 
+    add_action('admin_menu', function () {
+        add_menu_page( 'Post Favorito', 'Post Favorito', 'manage_options', 'posts-favorito', function () {
+            echo '
+                <h1 class="wp-heading-inline">Post Favorito</h1> <br />
+                Use shortcode: <b>[postfavorito itenspagina="5"]</b> <br />
+                O padrão de itens por pagina é 5 caso não informado
+            ';
+        }, 'dashicons-star-filled');
+    });
 
-    add_action( 'rest_api_init', function () use ($usuarioId, $logado, $tabela, $wpdb) {
+    add_shortcode('postfavorito', function ($param) {
+        $itenspagina = isset($param['itenspagina']) ? $param['itenspagina'] : 5;
+
+        return '
+            <ul data-id="post-favorito-itens"></ul>
+
+            <button onclick="carregarPost(this)" data-pagina="1">Carregar posts</button>
+
+            <script>
+            function carregarPost(el) {
+                    
+                el.innerHTML = \'Carregando...\';
+                var pagina = parseInt(el.getAttribute(\'data-pagina\'));
+
+                fetch(\'' . get_site_url() . '/wp-json/post-favorito/v1/posts/\' + pagina + \'/'. $itenspagina .'\')
+                .then(function(res) {
+
+                    if (!res.ok) {
+                        throw Error(res.statusText);
+                    }
+
+
+                    if( pagina === 1 && ' . $itenspagina . ' ) {
+                    
+                    }
+
+                    el.innerHTML = \'Carregar posts\';
+                    return res.json();
+
+                }).then(function(json) {
+                    var posts =  document.querySelector(\'ul[data-id=post-favorito-itens]\');
+                    
+                    if(json.length === 0) {
+                        el.remove();
+                    }
+
+                    if(pagina === 1 && json.length === 0) {
+                        var li = document.createElement(\'li\');
+                        li.innerHTML = \'Nenhum post favorito\';
+
+                        posts.appendChild(li);
+                        return;
+                    }
+
+                    el.setAttribute(\'data-pagina\', pagina + 1);
+
+                    json.forEach(function (item) {
+                    
+                        var a = document.createElement(\'a\');
+                        a.innerHTML = item.post_title;
+                        a.setAttribute(\'href\', \'' . get_site_url() . '/\' + item.post_name);
+
+                        var li = document.createElement(\'li\');
+                        li.appendChild(a);
+
+                        posts.appendChild(li);
+                    });
+                })
+                .catch(function() {
+                    el.innerHTML = \'Carregar posts\' ;
+
+                    alert(\'Erro ao carregar posts, tente novamente mais tarde!\');
+                });
+            }
+            </script>
+        ';
+    });
+
+    add_action( 'rest_api_init', function () use ($usuarioId, $tabela, $wpdb) {
+
+
+        register_rest_route( 'post-favorito/v1', 'posts/(?P<pagina>\d+)/(?P<limite>\d+)', array(
+            'methods' => 'GET',
+            'callback' => function ($param) use ($usuarioId, $tabela, $wpdb) {
+
+                $tabelaPost = $wpdb->prefix . 'posts';
+                $pagina = ($param->get_param('pagina') - 1)  * $param->get_param('limite');
+                $limite = $param->get_param('limite') + $pagina;
+                
+                $posts = $wpdb->get_results("
+                    SELECT 
+                        p.ID,
+                        p.post_name,
+                        p.post_title
+                    FROM {$tabelaPost} as p
+                    WHERE p.post_type = 'post' 
+                    AND p.post_status = 'publish'
+                    AND p.ID IN(SELECT u.post_id FROM {$tabela} u WHERE u.usuario_id = {$usuarioId})
+                    LIMIT $pagina, $limite
+                ");
+
+                return $posts;
+            },
+        ));
+
         
-        register_rest_route( 'post_favorito/v1', 'add/(?P<id>\d+)', array(
+        register_rest_route( 'post-favorito/v1', 'add/(?P<id>\d+)', array(
             'methods' => 'POST',
-            'callback' => function ($param) use ($usuarioId, $logado, $tabela, $wpdb) {
+            'callback' => function ($param) use ($usuarioId, $tabela, $wpdb) {
 
                 $postTipo = get_post_type($param->get_param('id'));
 
@@ -68,9 +171,9 @@ add_action('plugins_loaded', function() {
         ));
 
 
-        register_rest_route( 'post_favorito/v1', 'del/(?P<id>\d+)', array(
+        register_rest_route( 'post-favorito/v1', 'del/(?P<id>\d+)', array(
             'methods' => 'DELETE',
-            'callback' => function ($param) use ($usuarioId, $logado, $tabela, $wpdb) {
+            'callback' => function ($param) use ($usuarioId, $tabela, $wpdb) {
                 $dados = [
                     'usuario_id' => $usuarioId,
                     'post_id' => $param->get_param('id')
@@ -92,6 +195,7 @@ add_filter('the_content', function ($postConteudo) {
 
         global $wpdb;
         $post = get_post();
+
     
 
         $postFavorito = $wpdb->get_results( "
@@ -104,10 +208,10 @@ add_filter('the_content', function ($postConteudo) {
 
         return $postConteudo . '<p>
         
-            <button onclick="post_favorito(' . $post->ID . ', this)">' . $label . '</button>
+            <button onclick="postFavorito(' . $post->ID . ', this)">' . $label . '</button>
 
             <script> 
-                function post_favorito(id, el) {
+                function postFavorito(id, el) {
                     
                     var metodo = \'POST\';
                     var url  = \'add\';
@@ -119,7 +223,7 @@ add_filter('the_content', function ($postConteudo) {
 
                     el.innerHTML = \'Carregando...\';
 
-                    fetch(\'' . get_site_url() . '/wp-json/post_favorito/v1/\' + url + \'/\'  + id, {method: metodo})
+                    fetch(\'' . get_site_url() . '/wp-json/post-favorito/v1/\' + url + \'/\'  + id, {method: metodo})
                     .then(function(res) {
 
                         if (!res.ok) {
@@ -133,12 +237,13 @@ add_filter('the_content', function ($postConteudo) {
                         el.innerHTML = (url === \'del\') ? 
                             \'Remover post dos favoritos\' : \'Adicionar post aos favoritos\';
 
-                        alert(\'Erro ao adicionar post aos favoritos tente novamente mais tarde!\');
+                        alert(\'Erro ao adicionar post aos favoritos, tente novamente mais tarde!\');
                     });
                 }
             </script>
         </p>';
     }
+
 
     return $postConteudo;
 
