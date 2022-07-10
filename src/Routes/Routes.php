3367ad71b01a8e21641930a4ad29ffd25a9fbc23
Luis Paiva <contato@luispaiva.com.br>
2022-07-10 04:23:21 -0300

    Criação de classe para carregamento das rotas necessárias.

---
+++ src/Routes/Routes.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * WordPress Back-end Challenge.
+ *
+ * PHP version 7.4
+ *
+ * @category Challenge
+ * @package  WP_Backend_Challenge
+ * @author   Luis Paiva <contato@luispaiva.com.br>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/luispaiva/wordpress-back-end-challenge
+ */
+
+namespace App\Routes;
+
+/**
+ * Route class.
+ *
+ * @category Challenge
+ * @package  WP_Backend_Challenge
+ * @author   Luis Paiva <contato@luispaiva.com.br>
+ * @license  http://opensource.org/licenses/MIT MIT
+ * @link     https://github.com/luispaiva/back-end-challenge/tree/luis-paiva
+ */
+class Routes {
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		new Auth();
+		new Favorite();
+	}
+}
