dd671d5e41ab313575d76a4651104e6685a07027
Itamar Silva <itamarsilvacc@gmail.com>
2023-08-02 18:33:34 -0300

    Adicionar recurso de listar posts favoritos do usuário

--- favourites-controller.php
+++ favourites-controller.php
@@ -27,6 +27,16 @@ class FavouritesController {
                 'permission_callback' => array( 'FavouritesController', 'check_user_logged_in' ),
             )
         );
+
+        register_rest_route(
+            'wp-favourites/v1',
+            '/favourites',
+            array(
+                'methods' => 'GET',
+                'callback' => array( 'FavouritesController', 'get_user_favourites' ),
+                'permission_callback' => array( 'FavouritesController', 'check_user_logged_in' ),
+            )
+        );
     }
 
     /**
@@ -108,4 +118,39 @@ class FavouritesController {
 
         return (int) $result > 0;
     }
+
+    public static function get_user_favourites( $request ) {
+        $user_id = get_current_user_id();
+    
+        // Verificar se o usuário está logado
+        if ( empty( $user_id ) ) {
+            return new WP_Error( 'rest_forbidden', esc_html__( 'You are not authorized to access this endpoint.', 'wp-favourites' ), array( 'status' => 401 ) );
+        }
+    
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'wp_favourites';
+    
+        // Consulta para obter os IDs dos posts favoritos do usuário
+        $query = $wpdb->prepare(
+            "SELECT post_id FROM $table_name WHERE user_id = %d",
+            $user_id
+        );
+    
+        $post_ids = $wpdb->get_col( $query );
+    
+        // Preparar uma lista de posts favoritos com ID, título e data
+        $favourites = array();
+        foreach ( $post_ids as $post_id ) {
+            $post = get_post( $post_id );
+            if ( $post ) {
+                $favourites[] = array(
+                    'ID' => $post_id,
+                    'title' => get_the_title( $post_id ),
+                    'date' => $post->post_date,
+                );
+            }
+        }
+    
+        return rest_ensure_response( $favourites );
+    }    
 }
